4f0dd9a6b18c99699215cb4c9ee3aca1b795a5ea
Neto Lima <neto_lima@live.com>
2016-01-20 22:14:57 -0300

    Final commit before merging develop branch into master branch. I don't know why those files were not added in the last commit.

---
+++ webapp/financas.js
@@ -0,0 +1,8 @@
+
+   {
+      "hoje":"2678",
+      "ontem":"1768",
+      "setedias":"18274",
+      "trintadias":"75318",
+      "noventadias":"148774"
+   }
---
+++ webapp/iptu.js
@@ -0,0 +1,19 @@
+{  
+   "iptu":[ 
+      {  
+         "ano": "2015",
+         "valor":"213.98",
+         "situacao":"Pago"
+      },
+      {  
+         "ano": "2014",
+         "valor":"213.98",
+         "situacao":"Pago"
+      },
+      {
+         "ano": "2013",
+         "valor":"213.98",
+         "situacao":"Pago"
+      }
+   ]
+}
